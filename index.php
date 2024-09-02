5636a30ced4b682d3d9475e2e0162227a68bee02
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-09-02 10:08:24 +0300

    added

--- index.php
+++ index.php
@@ -1,3 +1,61 @@
 <?php
-echo 'test';
+    $company__logo = './style/assets/company_logo.svg';
+     function company_logo(){
+        echo './style/assets/company_logo.svg';
+    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login  A-Group</title>
+
+    <link rel="stylesheet" href="./style/style.css">
+
+    <link rel="shortcut icon" href="<?php company_logo() ?>" type="image/x-icon">
+</head>
+<body>
+    <div class="login__container">
+        <form action="./vendor/login.php" class="form__container">
+                <div class="form__image">
+                    <img src="<?php company_logo() ?>" alt="">
+                </div>
+
+                <div class="form__input__container">
+                    <p class="form__input__desc">
+                    userName:
+                    </p>
+                    <input type="text" name="username" id="username">
+                </div>
+
+                <div class="form__input__container">
+                    <p class="form__input__desc">
+                        password:
+                    </p>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div class="form__input__container">
+                    <p class="form__input__desc">
+                        pinCode:
+                    </p>
+                    <input type="text" name="pinCode" id="pinCode">
+                </div>
+
+                <div class="form__input__container">
+                    <p class="form__input__desc">
+                        policyNumber:
+                    </p>
+                    <input type="text" name="policyNumber" id="phoneNumber">
+                </div>
+                <div class="form__input__container">
+                    <p class="form__input__desc">
+                    phoneNumber:
+                    </p>
+                    <input type="text" name="phoneNumber" id="phoneNumber">
+                </div>
+        </form>
+    </div>
+</body>
+</html>
